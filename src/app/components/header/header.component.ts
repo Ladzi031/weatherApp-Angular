4ce3f20611eb14f1597d94c101f9f95feb3e8a62
Ladzi <ladziladzani@gmail.com>
2023-12-14 03:07:33 +0200

    a basic input 'cityName' validator

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Notify } from 'notiflix';
+import { validateCityName } from 'src/app/utils/cityNameValidate';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  cityName: string = "ladzi";
+  cityName: string = "";
+  constructor() {
+
+  }
+
+  getWeather() {
+    if (validateCityName(this.cityName) && this.cityName.length !== 0) {
+      // call weatherservice,  to lowercase
+      Notify.success("correct fetchingdata");
+
+      /*something interesting:
+      city with one of the shortest name in the world is "Å" located in Norway.
+       */
+    } else {
+      Notify.failure("please double check the name of the city");
+    }
+  }
 
 }
