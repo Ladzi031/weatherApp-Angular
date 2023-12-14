c0e904947abd8616e954b796731812333e654d0e
Ladzi <ladziladzani@gmail.com>
2023-12-14 11:15:22 +0200

    improve regex

--- src/app/utils/cityNameValidate.ts
+++ src/app/utils/cityNameValidate.ts
@@ -1,4 +1,9 @@
 export function validateCityName(cityName: string): boolean {
-    let containsLettersOnly: RegExp = /^[a-zA-Z]+$/;
+    let containsLettersOnly: RegExp = /^[^\d\s]*[^\d][^\d\s]*$/;
     return containsLettersOnly.test(cityName);
 }
+// at one letter and no numbers, can also be more than one word (e.g new york)
+/*
+something interesting:
+  city with one of the shortest name in the world is "Å" located in Norway.
+*/
